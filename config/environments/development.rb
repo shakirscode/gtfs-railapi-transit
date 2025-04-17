2774f2a0ce385aef0e5fdbc18165210590acb2bb
iqskr <86521069+iqskr@users.noreply.github.com>
2025-04-17 21:22:27 +0530

    Update development.rb
    change the env variable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # Add Railway Domain
+  config.hosts << "gtfs-railapi-transit-production.up.railway.app"
+
   config.hosts = [
      IPAddr.new('0.0.0.0/0'),              # All IPv4 addresses.
      IPAddr.new('::/0'),                   # All IPv6 addresses.
